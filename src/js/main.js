f1e22230778ea6fd178aca77334668ea31e2029d
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-11-30 00:37:25 +0330

    fix windos Scroll

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ const mainNav = $.querySelector("#mainNav");
 
 document.addEventListener("scroll", function () {
   // console.log(document.documentElement.scrollTop)
-console.log(window.onscroll);
+console.log(window);
   if (window.scrollTop > 0) {
     mainNav.classList.add("bg-black");
   } else {
