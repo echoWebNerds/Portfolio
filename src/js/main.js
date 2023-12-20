eecc9a7ac82619118e950c95a58c7d8329e77c05
Zarpoosh <mzar67839@gmail.com>
2023-12-20 20:10:53 +0330

    fix the indents

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,25 @@
 const $ = document;
 const mainNav = $.querySelector("#mainNav");
-const switchElement=$.querySelector(".switch");
-const menuItems=$.querySelector("#menu");
-const collapse=$.querySelector("#collapse")
-const icon=$.querySelector("#toggleMenu")
+const switchElement = $.querySelector(".switch");
+const menuItems = $.querySelector("#menu");
+const collapse = $.querySelector("#collapse");
+const icon = $.querySelector("#toggleMenu");
 
-const menuIcon=$.querySelector(".icon")
+const menuIcon = $.querySelector(".icon");
 
-
-switchElement.addEventListener("click", function() {
+switchElement.addEventListener("click", function () {
   // console.log("dark")
   document.body.classList.toggle("dark");
-  if(document.body.className.includes("dark")){
+  if (document.body.className.includes("dark")) {
     localStorage.setItem("theme", "dark");
-  }else{
-    localStorage.setItem("theme" , "light");
+  } else {
+    localStorage.setItem("theme", "light");
   }
-
-})
-
+});
 
 document.addEventListener("scroll", function () {
   // console.log(document.documentElement.scrollTop)
-// console.log(window.s);
+  // console.log(window.s);
   if (window.scrollY > 0) {
     mainNav.classList.add("bg-black");
   } else {
@@ -31,28 +28,26 @@ document.addEventListener("scroll", function () {
 });
 
 // save theme in localstorage
-window.onload=function(){
-  let localStorageTheme=localStorage.getItem("theme")
+window.onload = function () {
+  let localStorageTheme = localStorage.getItem("theme");
   // console.log(localStorageTheme)
-  if(localStorageTheme==="dark"){
-    document.body.classList.add("dark")
+  if (localStorageTheme === "dark") {
+    document.body.classList.add("dark");
   }
-}
+};
 
 function iconHandler() {
-  collapse.append(menuItems)
+  collapse.append(menuItems);
 
   if (menuItems.style.display === "none") {
     menuItems.style.display = "flex";
-    mainNav.classList.add("new-nav")
-    menuItems.classList.add("new-list")
-
+    mainNav.classList.add("new-nav");
+    menuItems.classList.add("new-list");
   } else {
     menuItems.style.display = "none";
-    mainNav.classList.remove("new-nav")
-    menuItems.classList.remove("new-list")
-
+    mainNav.classList.remove("new-nav");
+    menuItems.classList.remove("new-list");
   }
 }
 
-icon.addEventListener('click', iconHandler)
+icon.addEventListener("click", iconHandler);
